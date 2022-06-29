f6a34ab6c107aeac68e7f9c24e5b4c7ec9e56710
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-06-29 13:06:48 -0400

    completed find file script

--- find-file.sh
+++ find-file.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-LIST=( $(find / -name $1) )
-NUMLINES=( $(wc -l LIST) )
+LIST=( $(find . -name $1) )
+NUMLINES=( $(echo -n "$LIST" | grep -c '^') )
 
 if [[ $NUMLINES == 0 ]]
 then
-	echo "Found $NUMLINES matches.\n$LIST"
+	echo "Found 0 matches."
 elif [[ $NUMLINES != 0 ]]
 then
-	echo "Found 0 matches.\n$LIST"
+	echo "Found $NUMLINES matches.\n$LIST"
 fi
