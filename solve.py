d459f4307740e9b1329aee13e4d38bbb69febca5
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-18 15:44:56 -0400

    Update solve.py

--- solve.py
+++ solve.py
@@ -73,3 +73,18 @@ proc.stdin.flush()
 
 # Read an empty line from standard output
 print(proc.stdout.readline())
+
+# Read question 1 from standard output
+question_1 = proc.stdout.readline().decode("utf-8")
+print('\nReceived question 1: ', question_1)
+# Parse out the question and calculate the answer
+match_1=re.match(r"What is (\d+) \// (\d+)\?", question_1)
+answer_1 = str(int(match_1.group(1)) // int(match_1.group(2)))
+# Send answer + \n to standard input
+print('Sending answer: ', answer_1)
+proc.stdin.write(bytes(answer_1, 'utf-8'))
+proc.stdin.write(bytes('\n', 'utf-8'))
+proc.stdin.flush()
+
+# Read an empty line from standard output
+print(proc.stdout.readline())
