4cdbab6d946cbd9566d96e7b8cdf6a9acb2b097d
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-18 15:41:48 -0400

    Update solve.py

--- solve.py
+++ solve.py
@@ -44,30 +44,17 @@ print(proc.stdout.readline())
 # Read another line from standard output
 #print(proc.stdout.readline())
 
-# Read question 2 from standard output
-question_3 = proc.stdout.readline().decode("utf-8")
-print('\nReceived question 1: ', question_3)
+# Read question 1 from standard output
+question_1 = proc.stdout.readline().decode("utf-8")
+print('\nReceived question 1: ', question_1)
 # Parse out the question and calculate the answer
-
+match_1=re.match(r"What is (\d+) \+ (\d+)\?", question_1)
+answer_1 = str(int(match_1.group(1)) * int(match_1.group(2)))
 # Send answer + \n to standard input
-print('Sending answer: ', "nothing")
+print('Sending answer: ', answer_1)
 proc.stdin.write(bytes(answer_1, 'utf-8'))
 proc.stdin.write(bytes('\n', 'utf-8'))
 proc.stdin.flush()
 
 # Read an empty line from standard output
 print(proc.stdout.readline())
-# Read another line from standard output
-#print(proc.stdout.readline())
-
-# Read question 3 from standard output
-question_3 = proc.stdout.readline().decode("utf-8")
-print('\nReceived question 1: ', question_3)
-# Parse out the question and calculate the answer
-
-# Send answer + \n to standard input
-print('Sending answer: ', "n/a")
-proc.stdin.write(bytes(answer_1, 'utf-8'))
-proc.stdin.write(bytes('\n', 'utf-8'))
-proc.stdin.flush()
-
