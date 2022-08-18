49536f403b75782fcaf59b49e47a579692857f17
Laurel <41247144+laurelxiang@users.noreply.github.com>
2022-08-18 15:34:17 -0400

    Update solve.py

--- solve.py
+++ solve.py
@@ -45,15 +45,10 @@ print(proc.stdout.readline())
 #print(proc.stdout.readline())
 
 # Read question 2 from standard output
-question_2 = proc.stdout.readline().decode("utf-8")
-print('\nReceived question 1: ', question_2)
+question_3 = proc.stdout.readline().decode("utf-8")
+print('\nReceived question 1: ', question_3)
 # Parse out the question and calculate the answer
-#match_1=re.match(r"What is sqrt((\d+)\)?", question_1)
-#answer_1 = str(math.sqrt(int(match_1.group(1))))
-arr2 = question_2.split("(")
-arr2 = arr2[1].split(")")
-arr2 = arr2[0]
-answer_1 = str(math.sqrt(int(arr2)))
+
 # Send answer + \n to standard input
 print('Sending answer: ', answer_1)
 proc.stdin.write(bytes(answer_1, 'utf-8'))
